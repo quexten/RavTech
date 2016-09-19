5eea4c2be1686c7bd1ca18e6ebc2276083426964
Bernd Schoolmann <quextenmail@googlemail.com>
2016-09-19 20:45:38 +0200

    Add Buildreporterdialog Coloring

--- development-kit/src/com/quexten/ravtech/dk/ui/packaging/BuildReporterDialog.java
+++ development-kit/src/com/quexten/ravtech/dk/ui/packaging/BuildReporterDialog.java
@@ -2,24 +2,27 @@
 package com.quexten.ravtech.dk.ui.packaging;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.utils.Array;
 import com.kotcrab.vis.ui.VisUI;
+import com.kotcrab.vis.ui.util.highlight.Highlighter;
+import com.kotcrab.vis.ui.util.highlight.WordHighlightRule;
+import com.kotcrab.vis.ui.widget.HighlightTextArea;
+import com.kotcrab.vis.ui.widget.ScrollableTextArea;
 import com.kotcrab.vis.ui.widget.VisTable;
 import com.kotcrab.vis.ui.widget.VisTextArea;
 
 public class BuildReporterDialog extends VisTable {
 
 	public Array<PrinterListener> printerListeners = new Array<PrinterListener>();
-	VisTextArea textArea;
+	HighlightTextArea textArea;
 
 	public BuildReporterDialog () {
-		textArea = new VisTextArea() {
-			@Override
-			protected boolean continueCursor (int index, int offset) {
-				return false;
-			}
-		};
+		textArea = new HighlightTextArea("");
 		textArea.setDisabled(true);
+		Highlighter highlighter = new Highlighter();
+		highlighter.addRule(new WordHighlightRule(Color.GREEN, "BUILD SUCCESSFUL"));
+		textArea.setHighlighter(highlighter);
 		this.add(textArea).grow();
 	}
 
